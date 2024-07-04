ffaad981ef0f393ccae1cce50b46854c12b3b60e
josip <josipkeresman@gmail.com>
2024-07-04 22:34:14 +0200

    Simplify some stuff in map impl

--- source/headers/Map.h
+++ source/headers/Map.h
@@ -28,8 +28,6 @@ class Map
         uint32_t generateNumber(uint32_t t_max, uint32_t t_min);
         uint32_t countAliveNeighboursAtPosition(uint32_t t_positionX, uint32_t t_positonY);
 
-        bool isBorder(uint32_t t_rowIndex, uint32_t t_columnIndex);
-
         cell::State getRandomCellState(uint32_t t_criteriaForAlive);
 
     private:
