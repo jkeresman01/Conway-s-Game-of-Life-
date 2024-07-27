1e3084b58cf3b6ab0725fbf3d0214535d50bf841
josip <josipkeresman@gmail.com>
2024-07-27 23:32:31 +0200

    Add more dummy tests

--- test/MapTest.cpp
+++ test/MapTest.cpp
@@ -5,19 +5,34 @@
 
 using namespace gol;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) 
+{
     ::testing::InitGoogleTest(&argc, argv);
+
     return RUN_ALL_TESTS();
 }
 
-class MapTest : public ::testing::Test {
+class MapTest : public ::testing::Test 
+{
 protected:
     Map map;
+
     void SetUp() override {
         map.create(nullptr);
     }
 };
 
+TEST_F(MapTest, AfterMapIsCreated_WillCellsBeDeadInCurrentGeneration) 
+{
+    for(size_t i = 0; i < map::ROWS; ++i)
+    {
+        for(size_t j = 0; j < map::COLUMNS; ++j)
+        {
+            Cell cell = map.getCellAtPosition_CurrentGeneration(i, j);
+            EXPECT_EQ(cell.isAlive(), false);
+        }
+    }
+}
 TEST_F(MapTest, AfterMapIsCreated_WillCellsBeDeadInNextGeneration) 
 {
     for(size_t i = 0; i < map::ROWS; ++i)
